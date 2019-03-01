5f4d1af9dde2697b97489f37de405c7e246edd4e
Renan Moraes <root@bastion.democenter.cloud>
2019-03-01 12:47:43 -0300

    change6

--- www/push.sh
+++ www/push.sh
@@ -3,8 +3,8 @@ git add .
 sleep 5
 git commit -m $1
 sleep 5
-git push https://renanrmoraes:q1w2e3R$@github.com/renanrmoraes/hpa.git --all
+git push https://renanrmoraes:q1w2e3R%24@github.com/renanrmoraes/hpa.git --all
 #git push
-oc start-build hpa
-sleep 10
-oc get po -w
+#oc start-build hpa
+#sleep 10
+#oc get po -w
