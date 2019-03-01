1afc9a1cf6b3f4a11710ebadf499901df98529a3
Renan Moraes <root@bastion.democenter.cloud>
2019-03-01 11:22:57 -0300

    Change value 1 to 60

--- www/index.php
+++ www/index.php
@@ -21,20 +21,6 @@
 <p id="demo"></p>
 
 <script>
-//Teste Carga CPU
-function get_server_load()
-{
-    $fh = fopen('/proc/loadavg', 'r')
-    $data = fread($fh, 6);
-    fclose($fh);
-    $load_avg = explode(" ", $data);
-    return floatval(trim($load_avg[0]));
-}
-
-set_time_limit(120);
-while(get_server_load() > $max_load)
-    sleep($load_sleep_time);
-
 // Set the date we're counting down to
 var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
 
@@ -48,10 +34,10 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var days = Math.floor(distance / (1 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1 * 60 * 60 * 24)) / (1 * 60 * 60));
+  var minutes = Math.floor((distance % (60 * 60 * 60)) / (60 * 60));
+  var seconds = Math.floor((distance % (120 * 60)) / 60);
 
   // Display the result in the element with id="demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
