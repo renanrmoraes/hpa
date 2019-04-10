d4b667440bc6a84a2abc10665cdb1c05f55e4182
Renan Moraes <root@bastion.democenter.cloud>
2019-04-10 14:02:29 -0300

    path

---
+++ www/path1/hpa.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title>Hello world!</title>
+	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+	<style>
+	body {
+		background-color: white;
+		text-align: center;
+		padding: 50px;
+		font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
+	}
+
+	#logo {
+		margin-bottom: 40px;
+	}
+	</style>
+</head>
+  <body>
+    <?php
+        for($i = 0; $i < 500000000; $i++) {
+        }
+    ?>
+    <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
+  </body>
+</html>
