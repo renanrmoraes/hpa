34b7479b4ec0685794eed521ca189a05517aebbc
Renan Moraes <root@bastion.democenter.cloud>
2019-05-13 17:31:25 -0300

    v11

--- www/push.sh
+++ www/push.sh
@@ -4,6 +4,6 @@ sleep 5
 git commit -m $1
 sleep 5
 git push https://renanrmoraes:q1w2e3R%24@github.com/renanrmoraes/hpa.git --all
-oc start-build hpa
-sleep 10
-oc get po -w
+#oc start-build hpa
+#sleep 10
+#oc get po -w
