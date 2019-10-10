f05add76d973bbe58c6bfc34c6275c10347499c6
renanrmoraes <rmoraes@f9c.com.br>
2019-10-10 17:09:27 -0300

    Update upload.php

--- www/upload.php
+++ www/upload.php
@@ -3,10 +3,10 @@
 // de $_FILES.
 
 $uploaddir = '/uploads/';
-$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+$uploadfile = $uploaddir.$_FILES['file']['name'];
 
 echo '<pre>';
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
     echo "Arquivo válido e enviado com sucesso.\n";
 } else {
     echo "Possível ataque de upload de arquivo!\n";
